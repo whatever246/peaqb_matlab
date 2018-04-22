cf7d5f8e6d21f753213bee1461c1133c4e412fca
FFT-based ear model <37586343+audier@users.noreply.github.com>
2018-04-22 22:46:34 +0800

    Add files via upload

---
+++ Misc/PQwavFilePar.m
@@ -0,0 +1,37 @@
+function WAV = PQwavFilePar (File)
+% Print a WAVE file header, pick up the file parameters
+
+% P. Kabal $Revision: 1.1 $  $Date: 2003/12/07 13:34:11 $
+
+persistent iB
+
+if (isempty (iB))
+    iB = 0;
+else
+    iB = mod (iB + 1, 2);   % Only two files can be "active" at a time
+end
+
+% I think MATLAB wavread is funcitoning differently than octaves', I'll make
+% some modification here - SW 3.9.15
+
+[y, WAV.Fs] = audioread (File);
+WAV.Fname = File;
+WAV.Nframe = size(y,1);
+WAV.Nchan = 1;
+WAV.iB = iB;   % Buffer number
+
+%Hardcode:
+% WAV.Fs = 44100;
+
+% Initialize the buffer
+PQgetData (WAV, 0, 0);
+
+fprintf (' WAVE file: %s\n', File);
+if (WAV.Nchan == 1)
+    fprintf ('   Number of samples : %d (%.4g s)\n', WAV.Nframe, WAV.Nframe / WAV.Fs);
+else
+    fprintf ('   Number of frames  : %d (%.4g s)\n', WAV.Nframe, WAV.Nframe / WAV.Fs);
+end
+fprintf ('   Sampling frequency: %g\n', WAV.Fs);
+% fprintf ('   Number of channels: %d (%d-bit integer)\n', WAV.Nchan, Nbit);
+end
